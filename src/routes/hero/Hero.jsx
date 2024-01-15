32f431ab77651eec2930e923bc27723e82bc6e58
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-01-15 15:12:03 -0500

    hero and nav with animation
    hero was created and react spring applied to image for animating a rotation
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/routes/hero/Hero.jsx
+++ src/routes/hero/Hero.jsx
@@ -1,8 +1,47 @@
 import React from "react"
+import { useSpring, animated } from "@react-spring/web"
+import Cosmo from "../../images/cosmo.png"
 import "./hero.scss"
 
 const Hero = () => {
-  return <div>Hero</div>
+  const { rotate } = useSpring({
+    from: { rotate: 0 },
+    to: { rotate: 360 },
+    loop: true,
+    config: { duration: 400000 },
+  })
+
+  return (
+    <div>
+      <div className="hero-container">
+        <div className="hero-left-container">
+          <div className="hero-image">
+            <animated.div
+              style={{ transform: rotate.to((r) => `rotate(${r}deg)`) }}
+            >
+              <link
+                rel="preload"
+                href="path-to-your-image.jpg"
+                as="image"
+              ></link>
+              <img src={Cosmo} alt="cosmo" />
+            </animated.div>
+          </div>
+        </div>
+        <div className="hero-right-container">
+          <div className="mission-statement-container">
+            <span className="hekatek-header">
+              <span className="hekatek-word">HekaTek</span>
+              <br /> Web Development
+            </span>
+            <div className="mission-statement">
+              Your vision, our code. <br /> Freelance full stack web developers.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Hero
