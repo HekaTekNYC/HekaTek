0fdb0a53ed4d3857991ac87e4d64377c02757030
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-01-17 17:17:22 -0500

    about section set up
    about section background and space overlay with responsiveness implemented

--- src/routes/about/About.jsx
+++ src/routes/about/About.jsx
@@ -1,10 +1,6 @@
 import React from "react"
-import TechIcon from "../../components/tech-icon/Tech-icon"
 
-import { FaReact, FaNodeJs, FaCss3, FaHtml5, FaSass } from "react-icons/fa6"
-import { DiJavascript } from "react-icons/di"
-import { SiMongodb, SiExpress } from "react-icons/si"
-
+import SpaceOverlay from "../../components/stars/Stars"
 import "./about.scss"
 
 const About = () => {
@@ -13,7 +9,6 @@ const About = () => {
       <div className="about-text">
         <h1 className="about-header">Who We Are</h1>
         <div className="about-info">
-          <div className="icon"></div>
           We came together creating open source developer tools, driven by a
           shared passion for community. Our objective is clear: to transform
           your digital interfaces into engaging, efficient, and aesthetically
@@ -25,33 +20,11 @@ const About = () => {
           digital presence? Feel free to get in touch with us below.
         </div>
       </div>
-      {/* <div className="about-services">
-        <TechIcon iconColor="#05829b" iconText="React">
-          <FaReact />
-        </TechIcon>
-
-        <TechIcon iconColor="purple" iconText="Node">
-          <FaNodeJs />
-        </TechIcon>
-        <TechIcon iconColor="#1e2eda" iconText="CSS">
-          <FaCss3 />
-        </TechIcon>
-        <TechIcon iconColor=" #dac71e" iconText="Javascript">
-          <DiJavascript />
-        </TechIcon>
-        <TechIcon iconColor="#15a73c" iconText="MongoDB">
-          <SiMongodb />
-        </TechIcon>
-        <TechIcon iconColor="#db7226" iconText="HTML">
-          <FaHtml5 />
-        </TechIcon>
-        <TechIcon iconColor="#fff" iconText="Express">
-          <SiExpress />
-        </TechIcon>
-        <TechIcon iconColor="#c65394" iconText="Express">
-          <FaSass />
-        </TechIcon>
-      </div> */}
+      <div className="about-services">
+        <div className="star-overlay">
+          <SpaceOverlay />
+        </div>
+      </div>
     </div>
   )
 }
