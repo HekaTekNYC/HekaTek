cb4559c3b640bf4689b6d701daed72a4d96d5378
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-02-24 22:15:01 -0500

    V4 services
    services skeleton done except for width of individual items
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/routes/services/Services.jsx
+++ src/routes/services/Services.jsx
@@ -1,29 +1,29 @@
 import React from "react"
-import ServiceIcon from "../../components/service-icon/Service-icon"
+
 import "./services.scss"
 
 const servicesData = [
   {
-    name: "CODE OPTIMIZATION",
-    svgPath: "../../assets/icons/code.svg",
-    description:
-      "We review and refine existing codebases, enhancing their efficiency and readability. We focus on optimizing performance and ensuring best practices are followed.",
-  },
-  {
     name: "WEB DEVELOPMENT",
-    svgPath: "../../assets/icons/WebDev.svg",
+    svgPath: "../../assets/icons/website.svg",
     description:
       "We offer custom development services tailored to meet your unique needs, leveraging our expertise in various frameworks and JavaScript to create responsive websites.",
   },
   {
+    name: "CODE OPTIMIZATION",
+    svgPath: "../../assets/icons/solutions.svg",
+    description:
+      "We review and refine existing codebases, enhancing their efficiency and readability. We focus on optimizing performance and ensuring best practices are followed.",
+  },
+  {
     name: "DEBUGGING",
-    svgPath: "../../assets/icons/Debug2.svg",
+    svgPath: "../../assets/icons/bug.svg",
     description:
       "Our passion lies in tackling complex challenges and solving problems. Let's find some bugs and get them out of there!",
   },
   {
     name: "DIGITAL SOLUTIONS",
-    svgPath: "../../assets/icons/DigitalSolutions.svg",
+    svgPath: "../../assets/icons/solutions.svg",
     description:
       "Our range of digital services is custom-fit to your specific needs, ensuring quality and creativity from start to finish, with a keen eye on every detail.",
   },
@@ -33,14 +33,16 @@ const Services = () => {
   return (
     <div className="services-container">
       <div className="services-section-header">
-        <h1>SERVICES</h1>
-        {/* <img src="../../assets/images/Services.png" /> */}
+        <h2>SERVICES</h2>
       </div>
-      <div className="services-row">
+      <div className="services-card">
         {servicesData.map((service, index) => (
-          <div className="service-card" key={index}>
-            <ServiceIcon svgPath={service.svgPath} />
-            <div className="service-header">{service.name}</div>
+          <div key={index} className="service-info">
+            <div className="service-icon">
+              {" "}
+              <img src={service.svgPath} alt={service.alt} />
+            </div>
+            <h5 className="service-header">{service.name}</h5>
             <div className="service-text">{service.description}</div>
           </div>
         ))}
