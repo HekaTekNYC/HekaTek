c37d03bbc1c82db4f5e968e416d18a3598c858d4
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2024-03-08 14:33:28 -0700

    SVG Solution
    SVG Solution Implemented.
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
-const webpack = require("webpack");
-const CopyPlugin = require("copy-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-// const FontPreloadPlugin = require("webpack-font-preload-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+const webpack = require("webpack")
+const CopyPlugin = require("copy-webpack-plugin")
+const TerserPlugin = require("terser-webpack-plugin")
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // Import CSS minimizer plugin
 
 
-const mode = process.env.NODE_ENV === "production" ? "production" : "development";
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development"
 
 module.exports = {
   mode: mode,
@@ -21,7 +21,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|jpg|webp|gif|svg|ico|mp4|woff|woff2|)$/,
+        test: /\.(png|jpe?g|jpg|webp|gif|woff|woff2|eot|ttf|svg|ico|mp4)$/,
         use: [
           {
             loader: "file-loader",
@@ -32,24 +32,6 @@ module.exports = {
           },
         ],
       },
-      // {
-      //   test: /\.(woff|woff2|eot|ttf|otf)$/,
-      //   use: [
-      //     {
-      //       loader: 'file-loader',
-      //       options: {
-      //         name: '[name].[ext]',
-      //         publicPath: 'dist/assets/fonts/',
-      //       },
-      //     },
-      //   ],
-      // },
-  
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -77,6 +59,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
+    alias: {
+      three: path.resolve("./node_modules/three"),
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -84,47 +69,56 @@ module.exports = {
     }),
     new webpack.HotModuleReplacementPlugin(),
 
-
-    new TerserPlugin({
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-          pure_funcs: ['console.log']
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "src/assets",
+          to: "assets",
         },
-        mangle: {
-          toplevel: true
-        },
-        output: {
-          comments: false,
-          ascii_only: true
-        }
-      }
+        
+      ],
     }),
 
-    // Add CssMinimizerPlugin to minify CSS
-    new CssMinimizerPlugin(),
-    // new FontPreloadPlugin({
-    //   extensions: ["woff", "woff2", "ttf"],
-    //   crossorigin: true,
-    //   loadType: "preload",
-    //   families: [
-    //     {
-    //       family: "Be Vietnam Pro",
-    //       weights: [400],
-    //     },
-    //     {
-    //       family: "Bebas Neue",
-    //       weights: [400],
-    //     },
-    //     {
-    //       family: "Gothic A1",
-    //       weights: [400, 600, 700],
-    //     },
-    //   ],
-    // }),
-  ],
+  new TerserPlugin({
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log'] // Example of removing console.log calls
+      },
+      mangle: {
+        toplevel: true
+      },
+      output: {
+        comments: false,
+        ascii_only: true
+      }
+    }
+  }),
 
+
+  // Add CssMinimizerPlugin to minify CSS
+  new CssMinimizerPlugin(),
+  // new FontPreloadPlugin({
+  //   extensions: ["ttf"],
+  //   crossorigin: true,
+  //   loadType: "preload",
+  //   families: [
+  //     {
+  //       family: "Be Vietnam Pro",
+  //       weights: [400],
+  //     },
+  //     {
+  //       family: "Bebas Neue",
+  //       weights: [400],
+  //     },
+  //     {
+  //       family: "Gothic A1",
+  //       weights: [400, 600, 700],
+  //     },
+  //   ],
+  // }),
+],
 
   devServer: {
     static: {
@@ -140,119 +134,4 @@ module.exports = {
     },
     historyApiFallback: true,
   },
-};
-
-
-// const HtmlWebpackPlugin = require("html-webpack-plugin")
-// const path = require("path")
-// const webpack = require("webpack")
-// const CopyPlugin = require("copy-webpack-plugin")
-// const TerserPlugin = require("terser-webpack-plugin")
-// const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // Import CSS minimizer plugin
-
-
-// const mode =
-//   process.env.NODE_ENV === "production" ? "production" : "development"
-
-// module.exports = {
-//   mode: mode,
-//   entry: path.resolve(__dirname, "./src/index.jsx"),
-//   output: {
-//     filename: "[name].js",
-//     path: path.resolve(__dirname, "dist"),
-//     publicPath: "/",
-//   },
-
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(png|jpe?g|jpg|webp|gif|woff|woff2|eot|ttf|svg|ico|mp4)$/,
-//         use: [
-//           {
-//             loader: "file-loader",
-//             options: {
-//               name: "[name].[ext]",
-//               outputPath: "assets/",
-//             },
-//           },
-//         ],
-//       },
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: "babel-loader",
-//         },
-//       },
-//       {
-//         test: /\.css$/,
-//         use: ["style-loader", "css-loader"],
-//       },
-//       {
-//         test: /\.scss$/,
-//         use: [
-//           { loader: "style-loader" },
-//           { loader: "css-loader", options: { sourceMap: true } },
-//           { loader: "sass-loader", options: { sourceMap: true } },
-//         ],
-//       },
-//       {
-//         test: /\.json$/,
-//         type: "json",
-//       },
-//     ],
-//   },
-//   resolve: {
-//     extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
-//     alias: {
-//       three: path.resolve("./node_modules/three"),
-//     },
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "./public/index.html",
-//     }),
-//     new webpack.HotModuleReplacementPlugin(),
-
-//     new CopyPlugin({
-//       patterns: [
-//         {
-//           from: "src/assets",
-//           to: "assets",
-//         },
-        
-//       ],
-//     }),
-
-//   new TerserPlugin({
-//     terserOptions: {
-//       compress: {
-//         drop_console: true,
-//         drop_debugger: true,
-//         pure_funcs: ['console.log'] // Example of removing console.log calls
-//       },
-//       mangle: {
-//         toplevel: true
-//       },
-//       output: {
-//         comments: false,
-//         ascii_only: true
-//       }
-//     }
-//   }),
-// ], 
-//   devServer: {
-//     static: {
-//       directory: path.join(__dirname, "dist"),
-//     },
-//     headers: {
-//       "Cache-Control": "no-store",
-//     },
-//     hot: true,
-//     open: true,
-//     client: {
-//       overlay: false,
-//     },
-//     historyApiFallback: true,
-//   },
-// }
+}
