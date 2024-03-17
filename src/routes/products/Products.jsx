4c5ce2c192942132e0a01768b7a2145804e46875
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2024-03-16 18:06:34 -0600

    Added projects to Our Work
    added project cards
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

--- src/routes/products/Products.jsx
+++ src/routes/products/Products.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import ProductCard from "../../components/product-cards/ProductCard"
-import { plantHaus, interviewIQ, dangoDB, ad3lie } from "../../data/ProductData"
+import { plantHaus, aiSumz, riversEdge, interviewIQ, dangoDB, ad3lie } from "../../data/ProductData"
 import "./products.scss"
 
 const Products = () => {
@@ -12,7 +12,7 @@ const Products = () => {
     <div className="products-container">
       <h3>OUR WORK</h3>
       <div className="product-row">
-        {[plantHaus, interviewIQ, dangoDB, ad3lie].map((product) => (
+        {[plantHaus, aiSumz, riversEdge, interviewIQ, dangoDB, ad3lie].map((product) => (
           <div className="product-container" key={product.id}>
             <ProductCard
               {...product}
