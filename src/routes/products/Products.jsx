c19106bbe8f54323d5b1fd7149540a29ca67efc5
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-04-17 09:59:33 -0400

    product card background coloring

--- src/routes/products/Products.jsx
+++ src/routes/products/Products.jsx
@@ -15,13 +15,16 @@ const Products = () => {
   return (
     <div className="products-container">
       <h3>OUR WORK</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+        excepturi ad est perspiciatis vitae, rerum doloremque quaerat modi magni
+        quia.
+      </p>
       <div className="product-row">
         {[plantHaus, aiSumz, riversEdge, interviewIQ, dangoDB, ad3lie].map(
           (product) => (
             <div className="product-container" key={product.id}>
-              {/* <ProductCard
-              {...product}
-              /> */}
+              <ProductCard {...product} />
             </div>
           )
         )}
