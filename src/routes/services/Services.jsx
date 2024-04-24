557c5247ee4a13115a5170e984e5041f7dd891c3
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-04-24 12:13:27 -0400

    V5 - services edits
    V5 services edits to cards
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/routes/services/Services.jsx
+++ src/routes/services/Services.jsx
@@ -7,33 +7,46 @@ import "./services.scss"
 const Services = () => {
   return (
     <div className="services-container">
+      {" "}
       <div className="services-background">
-        <img src={ServiceBg} alt="" />
-      </div>
-      <h2 className="services-header">SERVICES</h2>
+        {" "}
+        <img src={ServiceBg} alt="" />{" "}
+      </div>{" "}
+      <h2 className="services-header">SERVICES</h2>{" "}
       <p className="services-p">
+        {" "}
         We offer a wide range of digital solutions to help you stand out online.
         From code optimization to complex problem-solving, our team loves a good
         challenge. Whatever your website needs, we've got the expertise to
-        deliver.
-      </p>
+        deliver.{" "}
+      </p>{" "}
       <div className="services-cards-container">
+        {" "}
         {servicesData.map((service, index) => (
-          <div key={index} className="service-card">
-            <div className="service-icon-container">
-              <img
-                src={service.svgPath}
-                alt={`${service.name} icon`}
-                loading="lazy"
-                decoding="async"
-                className="service-icon"
-              />
-            </div>
-            <h4 className="service-header"> {service.name}</h4>
-            <p className="service-text"> {service.description}</p>
+          <div className="service-card-container">
+            {" "}
+            <div key={index} className="service-card">
+              {" "}
+              <div className="service-icon-container">
+                {" "}
+                <img
+                  src={service.svgPath}
+                  alt={`$ {
+              service.name
+            }
+
+            icon`}
+                  loading="lazy"
+                  decoding="async"
+                  className="service-icon"
+                />{" "}
+              </div>{" "}
+              <h4 className="service-header"> {service.name}</h4>{" "}
+              <p className="service-text"> {service.description}</p>{" "}
+            </div>{" "}
           </div>
         ))}
-      </div>
+      </div>{" "}
     </div>
   )
 }
