f29a018619c8062dfa18922760a5a1c1fd85ea7e
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-04-25 20:34:26 -0400

    contact form connected
    final contact form with button connection and modal edit
    
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,24 +1,53 @@
+// import React from "react"
+// import "./button.scss"
+
+// const Button = ({ text, href, type, btnType, width }) => {
+//   const buttonProps = {}
+//   const buttonClass = `btn-${btnType === "solid" ? "solid" : "outline"}`
+//   const btnWidth = `btn-${width === "full" ? "full" : "short"}`
+//   if (type) {
+//     buttonProps.type = type
+//   }
+//   if (href) {
+//     buttonProps.href = href
+//   }
+
+//   return (
+//     <div className={`${buttonClass} ${btnWidth}`}>
+//       <a {...buttonProps} target="_blank">
+//         {text}
+//       </a>
+//     </div>
+//   )
+// }
+
+// export default Button
+
 import React from "react"
 import "./button.scss"
-
 const Button = ({ text, href, type, btnType, width }) => {
-  const buttonProps = {}
   const buttonClass = `btn-${btnType === "solid" ? "solid" : "outline"}`
   const btnWidth = `btn-${width === "full" ? "full" : "short"}`
-  if (type) {
-    buttonProps.type = type
-  }
-  if (href) {
-    buttonProps.href = href
-  }
 
-  return (
-    <div className={`${buttonClass} ${btnWidth}`}>
-      <a {...buttonProps} target="_blank">
+  if (href) {
+    return (
+      <div className={`${buttonClass} ${btnWidth}`}>
+        <a href={href} target="_blank" rel="noopener noreferrer">
+          {text}
+        </a>
+      </div>
+    )
+  } else {
+    const buttonType = type || "button"
+    return (
+      <button
+        type={buttonType}
+        className={`${buttonClass} ${btnWidth} btn-over`}
+      >
         {text}
-      </a>
-    </div>
-  )
+      </button>
+    )
+  }
 }
 
 export default Button
