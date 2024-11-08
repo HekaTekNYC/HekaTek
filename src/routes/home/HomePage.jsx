c6478bb69d0f532b446b86026cce40ec63988a1b
Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>
2024-11-08 06:43:56 -0500

    contact correction
    Co-Authored-By: Kat Athay <100489705+KatAthay@users.noreply.github.com>

--- src/routes/home/HomePage.jsx
+++ src/routes/home/HomePage.jsx
@@ -3,7 +3,7 @@ import Banner from "../../components/banner/Banner"
 import AboutSection from "../../sections/about-section/AboutSection"
 import ServicesSection from "../../sections/services-section/ServicesSection"
 import ProductsSection from "../../sections/products-section/ProductsSection"
-import ContactPage from "../contact/ContactPage"
+import ContactSection from "../../sections/contact-section/ContactSection"
 import WhyUsSection from "../../sections/why-us-section/WhyUsSection"
 import PricingSection from "../../sections/pricing-section/PricingSection"
 
@@ -32,7 +32,7 @@ const HomePage = () => {
         <PricingSection />
       </section>
       <section id="contact" className="contact-section">
-        <ContactPage />
+        <ContactSection />
       </section>
     </>
   )
