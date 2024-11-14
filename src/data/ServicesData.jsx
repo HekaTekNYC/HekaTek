89566c81ed057c617088fa2c2fd92af52b37836f
Kat Athay <100489705+KatAthay@users.noreply.github.com>
2024-11-14 07:14:07 -0700

    Services Page
    Created Services page
    
    Co-Authored-By: Celeste Knopf <75687483+DHolliday1881@users.noreply.github.com>

--- src/data/ServicesData.jsx
+++ src/data/ServicesData.jsx
@@ -4,7 +4,7 @@ export const servicesData = [
     svgPath: "../../assets/icons/service-gear.svg",
     description:
       "With 96.5% of internet traffic coming from mobile devices, we focus on optimizing your site to perform flawlessly on mobile.",
-    height: "75px",
+    height: "40px",
     width: "auto",
   },
   {
@@ -12,7 +12,7 @@ export const servicesData = [
     svgPath: "../../assets/icons/service-mobile.svg",
     description:
       "Our process always begins with a mobile-first approach, followed by modifications for tablet and desktop screens.",
-    height: "75px",
+    height: "40px",
     width: "auto",
   },
   {
@@ -20,7 +20,31 @@ export const servicesData = [
     svgPath: "../../assets/icons/service-responsive.svg",
     description:
       "Your website will be fully responsive, seamlessly adjusting on mobile, tablets, and desktops, accessible from any device.",
-    height: "75px",
+    height: "40px",
+    width: "auto",
+  },
+  {
+    name: "Optimized Code",
+    svgPath: "../../assets/icons/service-gear.svg",
+    description:
+      "With 96.5% of internet traffic coming from mobile devices, we focus on optimizing your site to perform flawlessly on mobile.",
+    height: "40px",
+    width: "auto",
+  },
+  {
+    name: "Mobile-First Design",
+    svgPath: "../../assets/icons/service-mobile.svg",
+    description:
+      "Our process always begins with a mobile-first approach, followed by modifications for tablet and desktop screens.",
+    height: "40px",
+    width: "auto",
+  },
+  {
+    name: "Fully Responsive",
+    svgPath: "../../assets/icons/service-responsive.svg",
+    description:
+      "Your website will be fully responsive, seamlessly adjusting on mobile, tablets, and desktops, accessible from any device.",
+    height: "40px",
     width: "auto",
   },
 ]
